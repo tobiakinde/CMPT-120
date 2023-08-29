809fd067736203afec1be8c50dd5d948003c5d40
Melissa Chodziutko <11694558+melissachodziutko@users.noreply.github.com>
2023-08-29 16:26:07 -0400

    Initial commit

---
+++ Assignment 1/helloworld.py
@@ -0,0 +1,16 @@
+#instructions: print out "Hello World!"
+#create a variable to hold your favorite color, then print out that variable with a string beforehand that says "My favorite color is: "
+#create a variable to hold your birthday (NO SLASHES, JUST THE DIGITS) and then print out "My birthday is: " with the variable
+
+
+
+def main():
+
+
+
+    
+    
+    
+    
+    
+main()
---
+++ Assignment 3/Debug.py
@@ -0,0 +1,50 @@
+def main():
+    
+    #Can you print out "Hello" 8 times? I gave you a tiny hint to start...
+    for x in range(?):
+        
+    #What about as a while loop?
+    loops = ?
+    while (loops < ?):
+        
+    
+    #The loop iterations are one behind in a non-programming counting way... how can we fix this?
+    count = 0
+    while (count < 3):   
+        print("While loop iteration", count)
+        count = count + 1
+        
+    #Same deal here!
+    for x in range(3):
+        print("For loop iteration:", x)
+     
+    #Uh oh I messed up and made an infinite loop... so silly of me!   
+    endless = 4    
+    while (endless < 5):
+        print("I'm stuck in this loop!!!!")
+    
+    #print out your last name one letter at a time
+    for x in "":
+        print(?)
+       
+     #aw i suck i made another infinite loop.. use that thing I taught you about to get out once it prints once... starts with a b... br....
+    found == False    
+    while found == False:
+        print("i only printed once")
+        
+    #can you fill this out so that it prints found when it hits the letter r?
+    for x in "Marist":
+        if ? == ?:
+            print("found")
+    
+    numbers = [1,2,3,4,5,6,7,8,9,10]
+    #you could print out the list using print(numbers) OR you could go the long way and use a for loop to print out the value of each index :)
+    for x in ?:
+        print(?[?])
+    
+    #what if I wanted you to print out only the even numbers in this range I made?
+    for x in range (20, 501):
+        #i feeeeel like modulooooooo is neededddd
+        if
+    
+main()
