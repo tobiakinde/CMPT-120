ceaa9580f855ca4355415f159d51695b383ba6cf
tobiakinde <143512511+tobiakinde@users.noreply.github.com>
2023-11-09 18:04:24 -0500

    nice

--- Assignment 4/Debug.py
+++ Assignment 4/Debug.py
@@ -2,46 +2,39 @@
 def printHello():
     print("Hello")
 
-def main():
-    printHello()
-main()
-    
-def printName(x):
-    print(x)
 
-def main():
-    printName("tobi")
-main()
-    
+def printName(x):
+    print("tobi")
+
 def addition(x, y):
     return(x + y)
-def main():
-    print(addition(10,4))
-    main()
+
 
 
 def smaller(i, j):
-
+    if i < j:
+        return(i)
+    if j < i:
+        return(j)
+    if j == i:
+        return(0)
     #if i is smaller than j, return i
     #if j is smaller than i, return j
     #if they're even, return 0
 
 def main():
-    
-    #call the printHello function here
-    
-    #call printName and give it the parameter of your name
+    print("hello")
 
 
     var1= 10
     var2= 20
     #What do we put in here to make it work?
-    print(addition())
+    print(addition(var1,var2))
     
     num1 = int(input("Enter number 1"))
     num2 = int(input("Enter number 2"))
     #what go here?
-    print(smaller())
+    print(smaller(num1,num2))
 
 
 
